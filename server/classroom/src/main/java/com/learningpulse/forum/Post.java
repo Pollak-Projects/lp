00c93b4817c023c19a62f1d28050f8e060df3a20
nezsha <nezsha@vern.cc>
2024-09-09 19:17:10 +0200

    chore: added gitignore for maven

--- server/classroom/src/main/java/com/learningpulse/forum/Post.java
+++ server/classroom/src/main/java/com/learningpulse/forum/Post.java
@@ -1,4 +1,4 @@
-package com.learningpulse.post;
+package com.learningpulse.forum;
 
 import jakarta.persistence.*;
 import lombok.*;
@@ -23,6 +23,7 @@ public class Post implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
+
     @GeneratedValue
     private UUID id; // id of the post itself
 
@@ -30,10 +31,10 @@ public class Post implements Serializable {
     private String message; // contents of the message
     private UUID sentBy; // id of the user that published it
     private String title; // title
-    
+
     @CreationTimestamp
     private Timestamp publishDate; // unix timestamp at time of creation
-    
+
     private List<Attachasdment> attachments; // list of attachments, either link or file
 
     // TODO: add list of replies

52c84aa23b00619e1b494375d726d4083a8390b5
hirst <hirsting@gmail.com>
2024-09-09 15:08:59 +0200

    fixed end of line characters to lf

--- web-client/app/dashboard/page.tsx
+++ web-client/app/dashboard/page.tsx
@@ -3,8 +3,7 @@ import { Button } from "@nextui-org/button";
 export default function Page() {
   return (
     <center>
-
       <Button color={"primary"}>Love me!</Button>
     </center>
-  )
+  );
 }
--- web-client/app/page.tsx
+++ web-client/app/page.tsx
@@ -1,10 +1,8 @@
 import { Button } from "@nextui-org/button";
 
-
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-
       <Button>Click me</Button>
     </section>
   );

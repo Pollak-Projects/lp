e0793dc66c1a38345b7d92c8e1f5450919f7802e
TGyAkos <85934789+TGyAkos@users.noreply.github.com>
2024-09-16 19:42:36 +0200

    added jwt auth converter #12 we just need a keycloak server to be able to test it now

--- server/gateway/src/main/java/com/learningpulse/gateway/config/AuthenticationConverter.java
+++ server/gateway/src/main/java/com/learningpulse/gateway/config/AuthenticationConverter.java
@@ -1,11 +1,13 @@
 package com.learningpulse.gateway.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.MethodParameter;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
 import java.util.Collection;
 import java.util.Map;
@@ -13,9 +15,8 @@ import java.util.stream.Collectors;
 
 public class AuthenticationConverter {
     @Bean
-    GrantedAuthoritiesMapper authenticaionConverter(
-            Converter<Map<String, Object>, Collection<GrantedAuthority>>
-            authoritiesConverter
+    GrantedAuthoritiesMapper authenticationConverter(
+            Converter<Map<String, Object>, Collection<GrantedAuthority>> authoritiesConverter
     ) {
         return (authorities) -> authorities.stream()
                 .filter(authority -> authority instanceof OidcUserAuthority)
@@ -25,5 +26,16 @@ public class AuthenticationConverter {
                 .map(authoritiesConverter::convert)
                 .flatMap(roles -> roles.stream())
                 .collect(Collectors.toSet());
+    }
+
+    @Bean
+    JwtAuthenticationConverter jwtAuthenticationConverter(
+            Converter<Map<String, Object>, Collection<GrantedAuthority>> authoritiesConverter
+    ) {
+        var authenticationConverter = new JwtAuthenticationConverter();
+        authenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            return authoritiesConverter.convert(jwt.getClaims());
+        });
+        return authenticationConverter;
     }
 }

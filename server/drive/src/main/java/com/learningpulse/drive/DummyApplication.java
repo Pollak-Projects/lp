cfecba81a3d0635c584ee9835054f8c3d61ce6e8
TGyAkos <85934789+TGyAkos@users.noreply.github.com>
2024-09-17 21:05:36 +0200

    added drive microservice #14 #15

---
+++ server/drive/src/main/java/com/learningpulse/drive/DummyApplication.java
@@ -0,0 +1,15 @@
+package com.learningpulse.drive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class DummyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DummyApplication.class, args);
+    }
+
+}

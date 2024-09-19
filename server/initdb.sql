f093284ebfa19c5691f2e9d88079e0cdf915671d
TGyAkos <85934789+TGyAkos@users.noreply.github.com>
2024-09-19 21:10:33 +0200

    automatic schema generation works #14 #15 #16

--- server/initdb.sql
+++ server/initdb.sql
@@ -1,19 +1,14 @@
-\c postgres;
-
 -- Create the keycloak database
 CREATE DATABASE keycloak;
 
 -- Connect to the keycloak database and create the schema
-\c keycloak;
+\connect keycloak;
 CREATE SCHEMA keycloak;
 
 -- Connect back to the default database to create the learning_pulse database
-\c postgres;
 CREATE DATABASE learning_pulse;
 
 -- Connect to the learning_pulse database and create the schemas
-\c learning_pulse;
-CREATE SCHEMA IF NOT EXISTS learning_pulse
-    AUTHORIZATION root;
-CREATE SCHEMA IF NOT EXISTS user_schema
-    AUTHORIZATION root;
+\connect learning_pulse;
+CREATE SCHEMA learning_pulse;
+CREATE SCHEMA user_schema;

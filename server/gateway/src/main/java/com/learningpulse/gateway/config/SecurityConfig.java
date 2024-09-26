2fd26fdfb19ffd62f7186bd124786830a5b951e1
nezsha <nezsha@vern.cc>
2024-09-26 22:48:08 +0200

    merged spring security

--- server/gateway/src/main/java/com/learningpulse/gateway/config/SecurityConfig.java
+++ server/gateway/src/main/java/com/learningpulse/gateway/config/SecurityConfig.java
@@ -97,11 +97,22 @@ public class SecurityConfig {
                 return jwt -> Mono.justOrEmpty(authenticationConverter.convert(jwt));
         }
 
-<<<<<<< HEAD
         @Bean
         AuthoritiesConverter realmRolesAuthoritiesConverter() {
                 return claims -> {
                         var realmAccess = Optional.ofNullable((Map<String, Object>) claims.get("realm_access"));
+                        // TODO finish this so we can get roles from here:
+                        // "resource_access": {
+                        // "client-credentials-test": {
+                        // "roles": [
+                        // "asd_role"
+                        // ]
+                        // },
+                        // var resourceAccess = Optional.ofNullable((Map<String, Object>) (((Map<String,
+                        // Object>) claims.get("resource_access"))
+                        // .get("user"))
+                        // .get("roles")
+                        // );
                         var roles = realmAccess.flatMap(map -> Optional.ofNullable((List<String>) map.get("roles")));
                         return roles.stream()
                                         .flatMap(Collection::stream)
@@ -111,30 +122,4 @@ public class SecurityConfig {
                                         .collect(Collectors.toList());
                 };
         }
-=======
-    @Bean
-    AuthoritiesConverter realmRolesAuthoritiesConverter() {
-        return claims -> {
-            var realmAccess = Optional.ofNullable((Map<String, Object>) claims.get("realm_access"));
-            // TODO finish this so we can get roles from here:
-            // "resource_access": {
-            //    "client-credentials-test": {
-            //      "roles": [
-            //        "asd_role"
-            //      ]
-            //    },
-//            var resourceAccess = Optional.ofNullable((Map<String, Object>) (((Map<String, Object>) claims.get("resource_access"))
-//                    .get("user"))
-//                    .get("roles")
-//            );
-            var roles = realmAccess.flatMap(map -> Optional.ofNullable((List<String>) map.get("roles")));
-            return roles.stream()
-                    .flatMap(Collection::stream)
-                    .map(roleName -> "ROLE_" + roleName)
-                    .map(SimpleGrantedAuthority::new)
-                    .map(GrantedAuthority.class::cast)
-                    .collect(Collectors.toList());
-        };
-    }
->>>>>>> b3dc936e97c9614308d1e50051f3df8cf26b8a61
 }

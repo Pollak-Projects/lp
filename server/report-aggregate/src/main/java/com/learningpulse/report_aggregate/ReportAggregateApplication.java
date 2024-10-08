71d81d1c9415d62d1c07501de37f69b251f5c437
TGyAkos <85934789+TGyAkos@users.noreply.github.com>
2024-10-08 22:48:42 +0200

    added test runner for dummy application and jacoco report aggregator

---
+++ server/report-aggregate/src/main/java/com/learningpulse/report_aggregate/ReportAggregateApplication.java
@@ -0,0 +1,11 @@
+package com.learningpulse.report_aggregate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportAggregateApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReportAggregateApplication.class, args);
+    }
+}

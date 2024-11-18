e2e45436328a97c3ea38d23dcba0ac31345a78d0
TGyAkos <85934789+TGyAkos@users.noreply.github.com>
2024-11-18 02:19:00 +0100

    fixed configs, started adding CORS and CSRF #14 #46

---
+++ web-client/next.config.ts
@@ -0,0 +1,31 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "standalone",
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: `${process.env.HOST_URL}`
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT,OPTIONS"
+          },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, x-auth-return-redirect"
+          }
+        ]
+      }
+    ];
+  }
+};
+
+export default nextConfig;

eba0e3516f98e1d3b6211e6be4c9bef60d1a7c2a
iwillsettyou <bmate20050911@gmail.com>
2024-11-26 09:57:45 +0100

    test

--- web-client/src/components/quizPage/quizSelectPage.tsx
+++ web-client/src/components/quizPage/quizSelectPage.tsx
@@ -13,30 +13,46 @@ export default function QuizSelectPage( userId:any) {
 
   const query = useGetAllQuizzes()
 
+let renderQuizProbe
 
-  if (query.isError) return query.error.message
+  if (query.isError) {
+    return <div className={"flex flex-row justify-center min-h-[100vh] relative"}>
+      <span className={"min-h-[100vh]"}>{query.error.message}</span>
+    </div>
+  }
+  else if (query.isLoading) {
+    return <div className={"flex flex-row justify-center min-h-[100vh] relative"}>
+      <Loading color={"default"} />
+    </div>
+  }
+  else if(query.data){
+    renderQuizProbe = query.data.map((quiz: any) => {
+      return (
+        <button key={quiz.id}>
+          <NextLink href={"quiz/" + quiz.id}>
 
-  if(query.isLoading) return <Loading color={"default"}/>
-
-  const renderQuiz = query.data.map((quiz: any) => {
-    return (
-      <button key={quiz.id}>
-        <NextLink href={"quiz/" + quiz.id}>
-
-          <div className={"flex flex-col bg-content2 w-[30dvw] rounded-lg " +
-            "min-h-[12dvh]"}
-          >
-            <span className={"text-2xl place-self-center"}>{quiz.name}</span>
-            {/* TODO: Implement name getting with keycloak
-            <span className={""}>Assigned by:{quiz.createdBy}</span>*/}
-            <span className={""}>{quiz.createdAt}</span>
-            <span className={""}>{quiz.deadline}</span>
-            <span className={"text-justify"}>{quiz.description}</span>
-          </div>
-        </NextLink>
-      </button>
-    )
+            <div className={"flex flex-col bg-content2 w-[30dvw] rounded-lg " +
+              "min-h-[12dvh]"}
+            >
+              <span className={"text-2xl place-self-center"}>{quiz.name}</span>
+              {/* TODO: Implement name getting with keycloak
+              <span className={""}>Assigned by:{quiz.createdBy}</span>*/}
+              <span className={""}>{quiz.createdAt}</span>
+              <span className={""}>{quiz.deadline}</span>
+              <span className={"text-justify"}>{quiz.description}</span>
+            </div>
+          </NextLink>
+        </button>
+      )
   })
+  }
+  else{
+    return <div className={"flex flex-row justify-center min-h-[100vh] relative"}>
+      <Loading color={"default"} />
+    </div>
+  }
+  //did the checking like this, because if you use a constant it will have a value, but if you were to map that value when the API gives bck nothing, yuo would get an exception, with this, the let won't have a value = no error
+  const renderQuiz = renderQuizProbe
 
 
 
